e57abeafa8701dbc0b1800efde8ca797343d3811
thomas <208098808+tlearm15@users.noreply.github.com>
2025-10-27 13:12:43 -0700

    Added Characters class

---
+++ RiverRiddle/models/Characters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RiverRiddle.Models
+{
+    public class Characters
+    {
+        public char RiverSide { get; set; } // 'L' for Left, 'R' for Right
+    }
+}
